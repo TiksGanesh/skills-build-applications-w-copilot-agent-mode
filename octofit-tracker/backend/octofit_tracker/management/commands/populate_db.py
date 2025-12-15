d2e2b8191ab04229ad132f3274f7135eb0ec0142
Ganesh Tikone <tikone.ganesh.p@gmail.com>
2025-12-15 08:00:08 +0000

    Fix serializers for string IDs and seed data with int PKs

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,62 +1,78 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth import get_user_model
-from django.db import connection
-from octofit_tracker.models import Team, Activity, Leaderboard, Workout
 
 class Command(BaseCommand):
     help = 'Populate the octofit_db database with test data'
 
     def handle(self, *args, **options):
-        User = get_user_model()
-        # Delete all data
-        User.objects.all().delete()
-        Team.objects.all().delete()
-        Activity.objects.all().delete()
-        Leaderboard.objects.all().delete()
-        Workout.objects.all().delete()
-
-
-        # Create Teams and save
-        marvel = Team(name='Marvel')
-        marvel.save()
-        dc = Team(name='DC')
-        dc.save()
-
-        # Create Users (superheroes) without team, then assign team and save
-        users = []
-        ironman = User.objects.create_user(username='ironman', email='ironman@marvel.com')
-        ironman.team = marvel
-        ironman.save()
-        users.append(ironman)
-
-        cap = User.objects.create_user(username='captainamerica', email='cap@marvel.com')
-        cap.team = marvel
-        cap.save()
-        users.append(cap)
-
-        batman = User.objects.create_user(username='batman', email='batman@dc.com')
-        batman.team = dc
-        batman.save()
-        users.append(batman)
-
-        superman = User.objects.create_user(username='superman', email='superman@dc.com')
-        superman.team = dc
-        superman.save()
-        users.append(superman)
-
-        # Create Activities
-        for user in users:
-            Activity.objects.create(user=user, type='run', duration=30, distance=5)
-            Activity.objects.create(user=user, type='cycle', duration=60, distance=20)
-
-        # Create Workouts
-        Workout.objects.create(name='Morning Cardio', description='Run and cycle combo', duration=90)
-        Workout.objects.create(name='Strength Training', description='Weights and resistance', duration=60)
-
-        # Create Leaderboard
-        Leaderboard.objects.create(team=marvel, points=100)
-        Leaderboard.objects.create(team=dc, points=90)
-
-        self.stdout.write(self.style.SUCCESS('Database populated with test data.'))
+        # Use direct MongoDB operations to bypass Djongo ForeignKey issues
+        from pymongo import MongoClient
+        from datetime import datetime
+        
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client['octofit_db']
+        
+        # Clear existing data
+        db.octofit_tracker_activity.delete_many({})
+        db.octofit_tracker_leaderboard.delete_many({})
+        db.octofit_tracker_workout.delete_many({})
+        db.octofit_tracker_user.delete_many({})
+        db.octofit_tracker_team.delete_many({})
+        
+        # Insert teams
+        marvel_id = 1
+        dc_id = 2
+        db.octofit_tracker_team.insert_many([
+            {'_id': marvel_id, 'name': 'Marvel'},
+            {'_id': dc_id, 'name': 'DC'}
+        ])
+        
+        # Insert users with all required AbstractUser fields
+        ironman_id = 1
+        cap_id = 2
+        batman_id = 3
+        superman_id = 4
+        
+        now = datetime.now()
+        user_template = {
+            'password': 'pbkdf2_sha256$390000$placeholder$unusable',
+            'last_login': None,
+            'is_superuser': False,
+            'first_name': '',
+            'last_name': '',
+            'is_staff': False,
+            'is_active': True,
+            'date_joined': now
+        }
+        
+        db.octofit_tracker_user.insert_many([
+            {**user_template, '_id': ironman_id, 'username': 'ironman', 'email': 'ironman@marvel.com', 'team_id': marvel_id},
+            {**user_template, '_id': cap_id, 'username': 'captainamerica', 'email': 'cap@marvel.com', 'team_id': marvel_id},
+            {**user_template, '_id': batman_id, 'username': 'batman', 'email': 'batman@dc.com', 'team_id': dc_id},
+            {**user_template, '_id': superman_id, 'username': 'superman', 'email': 'superman@dc.com', 'team_id': dc_id}
+        ])
+        
+        # Insert activities
+        activities = []
+        activity_id = 1
+        for user_id in [ironman_id, cap_id, batman_id, superman_id]:
+            activities.append({'_id': activity_id, 'user_id': user_id, 'type': 'run', 'duration': 30, 'distance': 5.0})
+            activity_id += 1
+            activities.append({'_id': activity_id, 'user_id': user_id, 'type': 'cycle', 'duration': 60, 'distance': 20.0})
+            activity_id += 1
+        db.octofit_tracker_activity.insert_many(activities)
+        
+        # Insert workouts
+        db.octofit_tracker_workout.insert_many([
+            {'_id': 1, 'name': 'Morning Cardio', 'description': 'Run and cycle combo', 'duration': 90},
+            {'_id': 2, 'name': 'Strength Training', 'description': 'Weights and resistance', 'duration': 60}
+        ])
+        
+        # Insert leaderboard
+        db.octofit_tracker_leaderboard.insert_many([
+            {'_id': 1, 'team_id': marvel_id, 'points': 100},
+            {'_id': 2, 'team_id': dc_id, 'points': 90}
+        ])
+            
+        self.stdout.write(self.style.SUCCESS('Database populated with test data using direct MongoDB inserts.'))
 
         # Unique index on email is enforced by Django model field
--- octofit-tracker/backend/octofit_tracker/serializers.py
+++ octofit-tracker/backend/octofit_tracker/serializers.py
@@ -1,30 +1,76 @@
 from rest_framework import serializers
 from .models import User, Team, Activity, Workout, Leaderboard
 
+
+def _stringify(value):
+    return str(value) if value is not None else None
+
+
 class TeamSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+
     class Meta:
         model = Team
-        fields = '__all__'
+        fields = ['id', 'name']
+
+    def get_id(self, obj):
+        return _stringify(obj.pk)
+
 
 class UserSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
     team = TeamSerializer(read_only=True)
+    team_id = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'team']
+        fields = ['id', 'username', 'email', 'team', 'team_id']
+
+    def get_id(self, obj):
+        return _stringify(obj.pk)
+
+    def get_team_id(self, obj):
+        return _stringify(obj.team_id)
+
 
 class ActivitySerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
     user = UserSerializer(read_only=True)
+    user_id = serializers.SerializerMethodField()
+
     class Meta:
         model = Activity
-        fields = '__all__'
+        fields = ['id', 'user', 'user_id', 'type', 'duration', 'distance']
+
+    def get_id(self, obj):
+        return _stringify(obj.pk)
+
+    def get_user_id(self, obj):
+        return _stringify(obj.user_id)
+
 
 class WorkoutSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
+
     class Meta:
         model = Workout
-        fields = '__all__'
+        fields = ['id', 'name', 'description', 'duration']
+
+    def get_id(self, obj):
+        return _stringify(obj.pk)
+
 
 class LeaderboardSerializer(serializers.ModelSerializer):
+    id = serializers.SerializerMethodField()
     team = TeamSerializer(read_only=True)
+    team_id = serializers.SerializerMethodField()
+
     class Meta:
         model = Leaderboard
-        fields = '__all__'
+        fields = ['id', 'team', 'team_id', 'points']
+
+    def get_id(self, obj):
+        return _stringify(obj.pk)
+
+    def get_team_id(self, obj):
+        return _stringify(obj.team_id)
